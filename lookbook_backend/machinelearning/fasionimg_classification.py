df52591659f0bd5fa08b35d647e0271e11ec93c4
jamin12 <rudals9901@naver.com>
2022-05-26 03:10:46 +0900

    Feat : Add multiprocessing and img resize in npyfile_make
    속도향상을 위한 멀티 프로세싱 등록과 이미지 resizing
        - npyfile_make.py : 이미지 resizing, 멀티프로세싱 등록
    fasionimg_calssification.py : 분류 작업중.....

--- lookbook_backend/machinelearning/fasionimg_classification.py
+++ lookbook_backend/machinelearning/fasionimg_classification.py
@@ -1,10 +1,13 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import re
 
-img : Image = Image.open("D:/K-Fashion/Validation/myorigindata/casual/street/390.jpg")
-imgArray : np.ndarray = np.array(img)
-imgArray_2d = imgArray.reshape(-1,800*800)
+imgArray = np.load('street4.npy',allow_pickle=True)
+print(imgArray[1].shape)
 
-plt.imshow(imgArray,cmap='gist_rainbow_r')
+imgArray_2d = imgArray[0].reshape(1201*800,-1)
+print(imgArray_2d.shape)
+
+plt.imshow(imgArray[2],cmap=plt.get_cmap('gray'))
 plt.show()
