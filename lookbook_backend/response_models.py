f3d4db1dfab93f19b2d375361b04e70c568b1f66
jamin12 <rudals9901@naver.com>
2022-06-13 02:03:03 +0900

    Feat : make response request models

--- lookbook_backend/response_models.py
+++ lookbook_backend/response_models.py
@@ -2,12 +2,12 @@ from typing import List
 from pydantic.main import BaseModel
 
 
-class get_tags_name(BaseModel):
+class res_tags_name(BaseModel):
     tag_name : List[str] = None
 
 
 class get_predict_age(BaseModel):
     predict_age : list = None
 
-class get_imgs(BaseModel):
+class res_imgs(BaseModel):
     imgs : List[str] = None
