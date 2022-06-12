94bbd1ca721d86b623cb786ddb5f29d819bd003b
jamin12 <rudals9901@naver.com>
2022-06-12 21:55:57 +0900

    Feat : add reponse imgs

--- lookbook_backend/routes/imgservice.py
+++ lookbook_backend/routes/imgservice.py
@@ -7,9 +7,11 @@ from typing import List
 sp.append(op.dirname(op.dirname(__file__)))
 
 from fastapi import UploadFile,File,APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse,JSONResponse
 from utils.date_utils import  D
 from machinelearning import face_recognize
+from utils import imgtobytes
+from PIL import Image
 
 IMG_DIR = "./lookbook_backend/machinelearning/img/"
 
@@ -29,6 +31,14 @@ async def uploadImg(in_files: List[UploadFile] = File(...)):
     result : dict = {"predict img" : predict_value}
     return result
 
-@router.get("/result",response_class=FileResponse)
+@router.get("/results")
 async def resultPage():
-    return ''.join([IMG_DIR,"test1.jpg"])
+    img_list = []
+    for i in range(1,3):
+        img_url = IMG_DIR + f'test{i}.jpg'
+        img = Image.open(img_url)
+        # img.show('img',img)
+        img_converted = await imgtobytes.from_image_to_bytes(img)
+        img_list.append(img_converted)
+    print(img_list)
+    return JSONResponse(img_list)
