00e063302f767a2cc8b84979c0b52046721ccd5e
jamin12 <rudals9901@naver.com>
2022-06-13 01:14:02 +0900

    FIX : response error 에러 해결

--- lookbook_backend/models.py
+++ lookbook_backend/models.py
@@ -3,12 +3,11 @@ from pydantic.main import BaseModel
 
 
 class get_tags_name(BaseModel):
-    # tag_name : List<String>
-    ...
+    tag_name : List[str] = None
 
 
 class get_predict_age(BaseModel):
     predict_age : list = None
 
 class get_imgs(BaseModel):
-    imgs : str = None
+    imgs : List[str] = None
